c49ca23fd4d149c577d814d8f1fbe9da3f2a28b6
unshahrul <147157365+unshahrul@users.noreply.github.com>
2025-07-15 14:53:26 +0800

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ReportForm from "@/components/ReportForm";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100">
-      <ReportForm />
-    </main>
-  )
-}
