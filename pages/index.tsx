2fc70aec6f5b75220414c47d28d854f346c03537
unshahrul <147157365+unshahrul@users.noreply.github.com>
2025-07-15 15:07:56 +0800

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+import ReportForm from "@/components/ReportForm"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      <ReportForm />
+    </main>
+  )
+}
